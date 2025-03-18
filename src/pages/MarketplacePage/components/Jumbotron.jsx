510dd4cff1f81bd6ac5f711055ebf34afa908487
Adham Muhammad <adhamfarid1110@gmail.com>
2025-03-18 23:49:10 +0700

    fix: navbar href

--- src/pages/MarketplacePage/components/Jumbotron.jsx
+++ src/pages/MarketplacePage/components/Jumbotron.jsx
@@ -14,10 +14,10 @@ const Jumbotron = () => {
                 </p>
                 <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                     <a
-                        href="#"
+                        href="/gimmick"
                         class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
                     >
-                        Start Buying
+                        Start Selling
                         <svg
                             class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                             aria-hidden="true"
@@ -35,7 +35,7 @@ const Jumbotron = () => {
                         </svg>
                     </a>
                     <a
-                        href="#"
+                        href="/gimmick"
                         class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
                         Browse Products
